1f6fec08b63680b2f380d2819f496526fffc648c
kimminsum <tommy1005a@gmail.com>
2024-05-24 16:10:39 +0900

    del crawling.py

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
 <!-- App.vue -->
 <template>
   <main>
-    <Sidebar />
-    <!-- <Navigation />
-    <Login /> -->
+    <!-- <Sidebar /> -->
+    <Navigation />
+    <Login />
   </main>
 </template>
 
 <script setup lang="ts">
 import Navigation from './components/Navigation.vue';
-import Sidebar from './components/Sidebar.vue';
 import Login from './components/Login.vue';
+// import Sidebar from './components/Sidebar.vue';
 </script>
